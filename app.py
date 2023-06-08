bc49e3373902b9f9cd554725e51d481753db0d62
root <root@aasheesh>
2023-06-08 18:14:55 +0100

    to be tested...

--- app.py
+++ app.py
@@ -7,7 +7,16 @@ from bin.filters import apply_filter
 app = Flask(__name__)
 
 # Read the PIL document to find out which filters are available out-of the box
-filters_available = []
+filters_available = [
+        "blur",
+        "contour",
+        "detail",
+        "edge-enchance",
+        "edge-enchance_more",
+        "emboss",
+        "sharpen",
+        "smooth",
+        "smooth_more" ]
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -16,7 +25,13 @@ def index():
     TODO:
     1. Return the usage instructions that specifies which filters are available, and the method format
     """
-    pass
+    response = {
+            "filters_available": filters_available,
+            "usage": {"http_method":"POST", "URL":"/<filter_available>/"},
+            }
+    return jsonify(response)
+
+    
 
 
 @app.post("/<filter>")
@@ -28,7 +43,15 @@ def image_filter(filter):
     3. Apply the filter using apply_filter() method from bin.filters
     4. Return the filtered image as response
     """
-    pass
+    if filter not in filters_available:
+        return jsonify({"error":"incorrect filter"})
+    # GETS THE IMAGE FILE
+    file = request.files["image"]
+    if not file:
+        return jsonify({"error":"no file provided"})
+    # APPLY THE IMAGE FILTER
+    filtered_image = apply_filter(file, filter)
+    return send_file(filtered_image, mimetype="image/JPEG")
 
 
 if __name__ == "__main__":
--- bin/filters.py
+++ bin/filters.py
@@ -12,5 +12,16 @@ def apply_filter(file: object, filter: str) -> object:
     4. Convert the PIL Image object to file object
     5. Return the file object
     """
+    # Open the as an PIL Image object
+    image = Image.open(file)
+    # 3. Apply the filter
+    image = image.filter(eval(f"ImageFilter.{filter.upper()}"))
+    # 4. Convert the PIL Image object to file object
+    # creates a new/empty bytes 'file' object
+    file = io.BytesIO()
+    # save the 'image' to the new 'file' object as 'JPEG'
+    image.save(file, "JPEG")
+    file.seek(0)
+    # 5. Return the file object
+    return file
 
-    pass
