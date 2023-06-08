59d94dca6ffeb026d6e56f35c12f72e4ace325e8
root <root@aasheesh>
2023-06-08 19:37:19 +0100

    multi-words dictionary

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def index():
 
 
 @app.get("/dict")
-def dictionary(word:str):
+def dictionary(words: List[str] = Query(None)):
     """
     DEFAULT ROUTE
     This method will
@@ -29,22 +29,35 @@ def dictionary(word:str):
     3. If not found, find all approximate matches and return
     """
     # 1. Accept a word from the request
-    if not word:
-        return jsonable_encoder({"status":"error",
-                                "word":word,
-                                "data":"word not found"})
-    definitions = match_exact(word)
-    if definitions:
-        return jsonable_encoder({"stats":"success",
-                                 "word":word,
-                                 "data":definitions})
-    definitions = match_like(word)
-    if definitions:
-        return jsonable_encoder({"stats":"partial",
-                                 "word":word,
-                                 "data":definitions})
-    else:
-        return jsonable_encoder({"status":"error",
-                                "word":word,
-                                    "data":"word not found"})
-
+    if not words:
+        return jsonable_encoder({
+            "status":"error",
+            "word":word,
+            "data":"word not found"
+        })
+    # initialize the response...
+    response = {"words": []}
+    for word in words:
+        definitions = match_exact(word)
+        if definitions:
+            response["words"].append({
+                "stats":"success",
+                "word":word,
+                "data":definitions
+            })
+        else:
+            # Try to find an approx match...
+            definitions = match_like(word)
+            if definitions:
+                response["words"].append({
+                    "stats":"partial",
+                    "word":word,
+                    "data":definitions
+                })
+            else:
+                response["words"].append({
+                    "status":"error",
+                    "word":word,
+                    "data":"word not found"
+                })
+    return jsonable_encoder(response)
