cefc51570e8e1ea9213fb024074d4c70e20ea14d
root <root@aasheesh>
2023-06-08 19:03:32 +0100

    OK

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+from typing import List
 from typing import Optional
 from fastapi import FastAPI
-from model.dbHandler import match_exact, match_like
+from fastapi import Query 
+from fastapi.encoders import jsonable_encoder
+from model.dbHandler import match_exact
+from model.dbHandler import match_like
 
-app = Flask(__name__)
+app = FastAPI()
 
 
 @app.get("/")
@@ -12,11 +16,11 @@ def index():
     This method will
     1. Provide usage instructions formatted as JSON
     """
-    return "TODO"
+    return jsonable_encoder({"usage":"/dict?=<word>"})
 
 
 @app.get("/dict")
-def dictionary():
+def dictionary(word:str):
     """
     DEFAULT ROUTE
     This method will
@@ -24,4 +28,23 @@ def dictionary():
     2. Try to find an exact match, and return it if found
     3. If not found, find all approximate matches and return
     """
-    return "TODO"
+    # 1. Accept a word from the request
+    if not word:
+        return jsonable_encoder({"status":"error",
+                                "word":word,
+                                "data":"word not found"})
+    definitions = match_exact(word)
+    if definitions:
+        return jsonable_encoder({"stats":"success",
+                                 "word":word,
+                                 "data":definitions})
+    definitions = match_like(word)
+    if definitions:
+        return jsonable_encoder({"stats":"partial",
+                                 "word":word,
+                                 "data":definitions})
+    else:
+        return jsonable_encoder({"status":"error",
+                                "word":word,
+                                    "data":"word not found"})
+
